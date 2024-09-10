4db8ba48af7dca35f22de095ae03335575a0fcb3
DMG <dgreatwood@gmail.com>
2024-09-10 11:53:42 -0700

    First Attemptate to Simulate Edge-Tirggered on Windows
    Individual Files
    ================
    
    pist_syslog.h
    Send all logging to stdout (for now, until we actually get Windows
    logging to work)
    
    pist_timelog.h
    Remove incorrect memory free for demangled name in Windows log
    Remove "class " or "struct " if part of demangled name for logging
    Correct bad cast to "const time_t *" for integer type that wasn't wide
    enough for time_t.
    
    ps_strl.h/.cc
    Add PS_STRNCPY_S
    
    client.cc
    In Windows, treat EWOULDBLOCK like EINPROGRESS on ::connect
    Add EVM_ET (edge trigger) to an event_new
    
    eventmeth.cc
    Don't throw if edge-triggered not supported
    Don't set EV_ET flag for event_new when EV_ET not supported
    Simulate egde-triggered by deactivating triggered events if
    edge-triggered not supported natively
    
    pist_syslog.cc
    Use AssumeEnabled macros when writing to Windows log
    
    https_server_test.cc
    Use PS_STRNCPY_S rather than strncpy_s to help it compile in Linux

--- src/common/ps_strl.cc
+++ src/common/ps_strl.cc
@@ -14,6 +14,8 @@
 #if defined(_IS_WINDOWS) || defined(__linux__)
 
 #include <string.h> // for memcpy
+#include <errno.h>
+#include <algorithm> // for std::min
 
 /* ------------------------------------------------------------------------- */
 
@@ -58,6 +60,68 @@ extern "C" size_t ps_strlcat(char *dst, const char *src, size_t n)
     return(dst_len+len_added);
 }
 
+
 /* ------------------------------------------------------------------------- */
 
 #endif // of #if defined(_IS_WINDOWS) || defined(__linux__)
+
+/* ------------------------------------------------------------------------- */
+
+
+/* ------------------------------------------------------------------------- */
+
+// ps_strncpy_s returns 0 for success, -1 on failure with errno set. NB: This
+// is different from C++ standard (Annex K) strncpy_s which returns an errno_t
+// on failure; we diverge because errno_t is often not defined on non-Windows
+// systems. If the copy would result in a truncation, errno is set to
+// PS_ESTRUNCATE.
+extern "C" int ps_strncpy_s(char *strDest, size_t numberOfElements,
+                            const char *strSource, size_t count)
+{
+#ifdef _IS_WINDOWS
+    errno_t win_strncpy_s_res = strncpy_s(strDest, numberOfElements,
+                                          strSource, count);
+    if (win_strncpy_s_res == 0)
+        return(0); // success
+
+    if (win_strncpy_s_res == STRUNCATE)
+    {
+        errno = PS_ESTRUNCATE;
+        return(-1);
+    }
+
+    errno = win_strncpy_s_res;
+    return(-1);
+#else // i.e. NOT Windows
+    if ((!strDest) || (!strSource))
+    {
+        errno = EINVAL;
+        return(-1);
+    }
+
+    size_t non_null_bytes_to_copy = std::min(strlen(strSource), count);
+    if (non_null_bytes_to_copy >= numberOfElements)
+    {
+        errno = PS_ESTRUNCATE;
+        return(-1);
+    }
+
+    memcpy(strDest, strSource, non_null_bytes_to_copy);
+    strDest[non_null_bytes_to_copy] = 0;
+    return(0);
+    
+#endif // of #ifdef _IS_WINDOWS
+}
+
+
+
+
+
+/* ------------------------------------------------------------------------- */
+
+
+
+/* ------------------------------------------------------------------------- */
+
+
+
