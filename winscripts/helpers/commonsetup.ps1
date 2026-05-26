697b4e606a138c6dbcfe26fab9668b229b915e26
DMG <dgreatwood@gmail.com>
2026-05-26 10:50:04 -0500

    Next Try

--- winscripts/helpers/commonsetup.ps1
+++ winscripts/helpers/commonsetup.ps1
@@ -93,13 +93,14 @@ cd ~
 
                     $dox_target = -join( `
                       "https://github.com/doxygen/doxygen/releases/download/Release_", `
-                      $ver_with_dots, `
+                      $ver_with_ubars, `
                       "/doxygen-", $ver_with_dots, `
                       ".windows.x64.bin.zip")
 
                     try {
-                        Invoke-WebRequest -Uri $dox_target `
-                          -OutFile doxygen.bin.zip -UserAgent "Mozilla/5.0"
+                        Invoke-WebRequest -Uri $dox_target -UseBasicParsing`
+                          -OutFile doxygen.bin.zip `
+                          -UserAgent "Mozilla/5.0"
 
                         if ($?) {
                             Write-Host "Fetching $dox_target returned success"
